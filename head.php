5e6670459f64b734f22c64a0c5e7ca5b5a759eab
Ryosuke Chinomi <opu.7455@gmail.com>
2019-09-10 18:02:46 +0900

    Create head.php

---
+++ head.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+  <html lang="ja">
+
+ 
+    <head>
+      <meta charset="utf-8">
+      <title> <?php echo $siteTitle; ?> | ÉPICE DE HOMME </title>
+      <link rel="stylesheet" type="text/css" href="style.css">
+      <link href="https://fonts.googleapis.com/css?family=Paytone+One&display=swap" rel="stylesheet">
+      <!-- フォントアイコン --> 
+      <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
+      <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+      <script src="https://code.jquery.com/ui/1.12.0/jquery-ui.js"></script>
+      <script type="text/javascript" src="js/effect.js"></script>
+    </head>
